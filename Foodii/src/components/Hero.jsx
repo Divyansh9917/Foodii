62cca2eca87cc9d156aebae087a13ee138c952f8
Divyansh9917 <divyanshm027@gmail.com>
2026-03-29 17:56:17 +0530

    fix: capital L in Logo folder path

--- Foodii/src/components/Hero.jsx
+++ Foodii/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroLogo from '../logo/herologo.svg'; 
+import heroLogo from '../Logo/herologo.svg'; 
 
 
 const Hero = () => {
