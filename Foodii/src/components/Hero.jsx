feb8a109fcf0e7cf1e3fc5b9134909079fe66189
Divyansh9917 <divyanshm027@gmail.com>
2026-03-29 17:46:35 +0530

    once more tried

--- Foodii/src/components/Hero.jsx
+++ Foodii/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroLogo from '../LOGO/herologo.svg'; 
+import heroLogo from '../logo/herologo.svg'; 
 
 
 const Hero = () => {
